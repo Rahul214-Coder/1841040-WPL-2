d43789447f3e870b077835c3502bc3b91ba8e127
Rahul <rahulponde@gmail.com>
2021-04-22 17:07:51 +0530

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("$$$$ HEllO WORLD $$$$ My PRN no 1841040 Welcome Back To My Website(/)")
